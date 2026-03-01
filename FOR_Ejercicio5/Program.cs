351f6170fae264f7806e05a2baf3486d13545472
dgTr3 <Edugta31@gmail.com>
2026-03-01 14:17:19 -0600

    completo

--- FOR_Ejercicio5/Program.cs
+++ FOR_Ejercicio5/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Mostrar una tabla de multiplicar del 1 al 5 (todas juntas).");
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
